87b5afc9061d096a09421bba64ca64a181ad24d6
guolh1 <gavin141113@163.com>
2019-02-22 21:40:38 +0800

    add fileListPlugin

--- plugins/FileListPlugin.js
+++ plugins/FileListPlugin.js
@@ -1,8 +1,23 @@
 class FlieListPlugin {
+    constructor({filename}) {
+        this.filename = filename
+    }
     apply(compiler) {
         // emit
-        compiler.hooks.emit.tapAsync('FileListPlugin', (compilation, cb) => {
-            console.log(compilation)
+        compiler.hooks.emit.tap('FileListPlugin', (compilation) => {
+            let assets = compilation.assets
+            let content = `## 文件名    资源大小\r\n`
+            Object.entries(assets).forEach(([filename, statObj]) => {
+                content += `- ${filename}    ${statObj.size()} bytes\r\n`
+            })
+            assets[this.filename] = {
+                source() {
+                    return content
+                },
+                size() {
+                    return content.length
+                }
+            }
         })
     }
 }
